99867c0f27a5f07da5cdef5b0ca2f77f4f6f265f
Julie <Julie@julie_nav>
2025-04-09 12:01:28 -0600

    same message

--- CreditCard/src/CreditCard.java
+++ CreditCard/src/CreditCard.java
@@ -15,7 +15,7 @@ public class CreditCard
 		public static void doubleDigits()
 		{
 			
-			int[] card = {5,4,2,4,1,3,0,1,2,3,4,5,6,7,8,9};
+			int[] card = {5,4,2,4,1,8,0,1,2,3,4,5,6,7,8,9};
 			int value;
 			
 			
@@ -62,9 +62,7 @@ public class CreditCard
 			 
 			 }
 			 System.out.print(realCard);
-			 
-			 
+			  
 		 }
-		
-		
+			
 	}
